a07ebced66898f89da9fe2a0be8b86cde6f9062d
AlexL70 <alexander.levinson.70@gmail.com>
2018-01-24 21:31:34 +0200

    Implement and check all usual conditions for string field

--- CombineExpressions/Program.cs
+++ CombineExpressions/Program.cs
@@ -8,41 +8,63 @@ namespace CombineExpressions
         static void Main(string[] args)
         {
             Expression<Func<User, string>> expr = (User u) => u.FirstName;
-            var combined = Combiner(expr, AdvSearchOperators.Contains, "John");
+            var searchStr = "John";
+            var combined = StringFieldPredicate(expr, AdvSearchOperators.Equals, searchStr);
             Console.WriteLine(combined.ToString());
+            combined = StringFieldPredicate(expr, AdvSearchOperators.NotEqual, searchStr);
+            Console.WriteLine(combined.ToString());
+            combined = StringFieldPredicate(expr, AdvSearchOperators.Contains, searchStr);
+            Console.WriteLine(combined);
+            combined = StringFieldPredicate(expr, AdvSearchOperators.NotContain, searchStr);
+            Console.WriteLine(combined);
+            Console.ReadKey();
         }
 
-        public static Expression<Func<User, bool>> Combiner(Expression<Func<User, string>> fieldExpr, AdvSearchOperators op,
+        public static Expression<Func<User, bool>> StringFieldPredicate(Expression<Func<User, string>> fieldExpr, AdvSearchOperators op,
             string str)
         {
+            var strExpr = Expression.Constant(str, typeof(string));
             Expression<Func<User, bool>> predicate;
 
             switch (op)
             {
-                    case AdvSearchOperators.Equals:
-                        predicate = Expression.Lambda<Func<User, bool>>(
-                            Expression.Equal(
-                                fieldExpr.Body,
-                                Expression.Constant(str, typeof(string))
+                case AdvSearchOperators.Equals:
+                    predicate = Expression.Lambda<Func<User, bool>>(
+                        Expression.Equal(
+                            fieldExpr.Body, strExpr
+                        ), fieldExpr.Parameters);
+                    break;
+
+                case AdvSearchOperators.NotEqual:
+                    predicate = Expression.Lambda<Func<User, bool>>(
+                        Expression.NotEqual(
+                            fieldExpr.Body, strExpr
                             ), fieldExpr.Parameters);
                     break;
 
                 case AdvSearchOperators.Contains:
+                case AdvSearchOperators.NotContain:
                     predicate = Expression.Lambda<Func<User, bool>>(
                         Expression.Call(fieldExpr.Body,
-                            typeof(String).GetMethod("Contains", new Type[] { typeof(string) }), 
-                            Expression.Constant(str, typeof(string))
+                            typeof(String).GetMethod("Contains", new Type[] { typeof(string) }),
+                            strExpr
                         ),
                         fieldExpr.Parameters
                     );
+                    if (AdvSearchOperators.NotContain == op)
+                    {
+                        predicate = Expression.Lambda<Func<User, bool>>(
+                            Expression.Not(predicate.Body), predicate.Parameters
+                            );
+                    }
                     break;
 
-                    default:
-                        predicate = u => true;
+                default:
+                    predicate = u => true;
                     break;
             }
 
             return predicate;
-        } 
+        }
     }
 }
